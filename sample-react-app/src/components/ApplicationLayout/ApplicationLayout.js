55035b1f4c4b5b04a0633d15848d178287041010
John David Martin <johndavidfive@gmail.com>
2019-09-18 15:09:34 -0700

    updating layout styles and text

--- sample-react-app/src/components/ApplicationLayout/ApplicationLayout.js
+++ sample-react-app/src/components/ApplicationLayout/ApplicationLayout.js
@@ -40,8 +40,8 @@ export default class ApplicationLayout extends React.Component {
             <div className="application-layout container">
 
               <div className="row">
-                <div className="col-lg-10 offset-lg-1 mb-4">
-                  <p>This demo illustrates how multiple <strong>StateStores</strong> can be created and used to generate a <strong>ReactSimpleStateContext</strong> that can be used across the app. It shows how to update shallow properties as well as how to update arrays.</p>
+                <div className="col-lg-10 offset-lg-1 mb-5 mt-3">
+                  <blockquote className="blockquote">This demo illustrates how multiple <span className="badge badge-light">StateStores</span> can be created and used to generate a <span className="badge badge-light">ReactSimpleStateContext</span> that can be used across the app. It shows how to update shallow properties as well as how to update arrays.</blockquote>
                 </div>
               </div>
 
@@ -49,7 +49,7 @@ export default class ApplicationLayout extends React.Component {
                 <div className="col-lg-6 mb-4">
                   <h2>Pass Down Context State</h2>
                   <p className="mb-4">
-                    The user card below is being passed "userState" that is being consumed from a parent. It also has internal component state to track the active tab. This example also shows how to use `asycState` to help with asynchronous calls to load a user's tweets.
+                    The user card below is being passed <span className="badge badge-light">userState</span> that is being consumed from a parent. It also has internal component state to track the active tab. This example also shows how to use <span className="badge badge-light">asycState</span> to help with asynchronous calls to load a user's tweets.
                   </p>
                   <h3>User Card Information</h3>
                   <UserCard user={userState} onTweetsRequested={() => { this.onUserTweetsRequested(); }}></UserCard>
@@ -57,7 +57,7 @@ export default class ApplicationLayout extends React.Component {
                 <div className="col-lg-6">
                   <h2>Use Context State Directly</h2>
                   <p className="mb-4">
-                    The Todos component below has it's own context consumer to access "todosState". It also has some internal state for the todo's input text.
+                    The Todos component below has it's own context consumer to access <span className="badge badge-light">todosState</span>. It also has some internal state for the todo's input text.
                   </p>
                   <h3>Your Todo List</h3>
                   <Todos></Todos>
